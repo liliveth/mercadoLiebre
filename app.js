f1c88b90d7b03cb1708dfdc84d25b1fe7394f6e2
liliveth <lili.49_@outlook.com>
2023-11-09 19:14:50 -0300

    agregando formularios

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./views/home.html"));
 });
 
+app.get("/registro", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/register.html"));
+});
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/login.html"));
+});
+
+
 app.use(express.static("public"));
 
 app.listen(3000, () => console.log("Levantando servidor en puerto 3000"));
